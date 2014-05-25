933093716937a145d9807b585c23ef24aa3e891d
Julien DIDIER <julien@didier.io>
2014-05-25 15:14:45 +0200

    fix paths

--- web/assets/js/services/Phone.js
+++ web/assets/js/services/Phone.js
@@ -2,7 +2,7 @@ define(['./module'], function (services) {
     'use strict';
 
     services.factory('Phone', ['$resource', function($resource){
-        return $resource('phones/:phoneId.json', {}, {
+        return $resource('/phones/:phoneId.json', {}, {
             query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
         });
     }]);
